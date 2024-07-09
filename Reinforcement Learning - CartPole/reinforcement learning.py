2aad3f60134937c02eac7c45536782e072142613
fatima204 <114151431+fatima204@users.noreply.github.com>
2024-07-09 22:24:25 +0200

    Update reinforcement learning.py

--- Reinforcement Learning - CartPole/reinforcement learning.py
+++ Reinforcement Learning - CartPole/reinforcement learning.py
@@ -7,7 +7,7 @@ from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.optimizers.legacy import Adam
 
 # install keras.rl version 1.5.0
-from rl.agents import DQNAgent  # pip install keras-rl2
+from rl.agents import DQNAgent  
 from rl.policy import BoltzmannQPolicy
 from rl.memory import SequentialMemory
 
@@ -16,6 +16,7 @@ from rl.memory import SequentialMemory
 # pygame to display the visualisation
 
 env = gym.make("CartPole-v1")  # no render mode to prevent display while training
+# when you add this line rendermode = 'human', then you can see the training
 
 states = env.observation_space.shape[0]
 actions = env.action_space.n
@@ -42,7 +43,7 @@ print(env)
 print(env.observation_space)
 
 agent.compile(Adam(lr=0.001), metrics=["mae"])
-agent.fit(env, nb_steps=100000, visualize=False, verbose=1)
+agent.fit(env, nb_steps=100000, visualize=False, verbose=1) # and you have to set visualize=True to see the training
 
 results = agent.test(env, nb_episodes=10, visualize=True)
 print(np.mean(results.history["episode_reward"]))
